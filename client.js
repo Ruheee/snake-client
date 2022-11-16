5ab2d92aed95ab165f696799b0e0ff516fd1303a
Rui Dantas <ruidantas55@gmail.com>
2022-11-16 20:25:24 +0000

    Display name and message notifying other users you have joined

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541 
+  });
+
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log(data);
+
+  });
+  conn.on("connect", () => {
+    conn.write("Name: Rui")
+    console.log("Successfully connected to game server")
+  })
+
+  conn.setEncoding("utf8");
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541 
-  });
-
-  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log(data)
-
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-}
-
+const { connect } = require("./client.js");
+ 
 console.log("Connecting ...");
 connect();
